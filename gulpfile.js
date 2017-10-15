7a04cb867179c485436852b30e9fc1ccb5b6c539
Paul <sicmusicmu@gmail.com>
2017-10-15 11:54:06 +0200

    Installed packages

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function () {
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
+})
+
+gulp.task('watch', function () {
+  watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('styles');
+  });
+})
